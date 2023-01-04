8f2b34ec273bddf67de14454cd45f38a7864235d
mitsubosh <yuya.mitsuboshi@gmail.com>
2023-01-04 17:00:39 +0900

    Implement base interface

--- lib/etoji.rb
+++ lib/etoji.rb
@@ -1,8 +1,36 @@
 # frozen_string_literal: true
 
-require_relative "etoji/version"
+require "etoji/version"
+require "etoji/jikkan"
+require "etoji/jyunishi"
 
 module Etoji
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.geto(year:, with_prev: false, with_next: false)
+    year_int = nil
+    begin
+      year_int = Integer(year)
+    rescue
+      raise ::Etoji::Error.new("Invalid year is given, #{year}")
+    end
+
+    jyunishi_number = (year_int - 3) % 12
+    current_animal = ::Etoji::Jyunishi.find_by_number(jyunishi_number)
+
+    case [with_prev, with_next]
+    in [true, true]
+      prev_animal = ::Etoji.geto(year: year_int - 1)
+      next_animal = ::Etoji.geto(year: year_int + 1)
+      [prev_animal, current_animal, next_animal]
+    in [true, false]
+      prev_animal = ::Etoji.geto(year: year_int - 1)
+      [prev_animal, current_animal]
+    in [false, true]
+      next_animal = ::Etoji.geto(year: year_int + 1)
+      [current_animal, next_animal]
+    else
+      current_animal
+    end
+  end
 end
---
+++ lib/etoji/jyunishi.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+require 'json'
+
+module Etoji
+  module Jyunishi
+    Animal = Data.define(:number, :emoji, :character, :character_hiragana, :animal_name_ja, :animal_name_ja_hiragana, :animal_name_en)
+
+    SOURCE_FILE_PATH = File.expand_path('../../../db/jyunishi.json', __FILE__)
+    MEMBERS = JSON.parse(File.read(SOURCE_FILE_PATH), symbolize_names: true).map { |attributes|
+      Animal.new(
+        number: attributes[:number],
+        emoji: attributes[:emoji],
+        character: attributes[:character],
+        character_hiragana: attributes[:character_hiragana],
+        animal_name_ja: attributes.dig(:animal_name, :ja),
+        animal_name_ja_hiragana: attributes.dig(:animal_name, :ja_hiragana),
+        animal_name_en: attributes.dig(:animal_name, :en)
+      )
+    }.freeze
+
+    def self.all
+      MEMBERS
+    end
+
+    def self.find_by_number(number)
+      @animals_by_number ||= MEMBERS.each_with_object({}) { |animal, result| result[animal.number] = animal }
+      @animals_by_number[number]
+    end
+  end
+end
